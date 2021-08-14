b86cecf4ec93a39b4876f12d27fff84e7d6bdd65
Nisith Pati <54003224+patink@users.noreply.github.com>
2021-08-14 20:25:41 +0530

    Add files via upload

---
+++ dataaug.py
@@ -0,0 +1,32 @@
+from keras.preprocessing.image import ImageDataGenerator,load_img,img_to_array
+
+#data augmentation
+
+datagen = ImageDataGenerator(
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    rescale = 1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest')
+#class 3
+pic = load_img('C:/Users/lenovo/Desktop/ARCHIVE/HAM10000_images_part_1/ISIC_0029297.jpg')
+pic.getpixel
+pic_array=img_to_array(pic)
+pic_array.shape
+
+pic_array = pic_array.reshape((1,)+pic_array.shape)
+pic_array.shape
+
+
+# Generate 10 images
+# batch_size: I can reduce it if lesion images are more so that it can be diversified
+count = 0
+for batch in datagen.flow(pic_array, batch_size=1,save_to_dir="C:/Users/lenovo/Desktop/ARCHIVE/HAM10000_images_part_3", save_prefix='ISIC', save_format='jpg'):
+    count += 1
+    if count == 10:
+        break
+    
+
---
+++ main.py
@@ -0,0 +1,303 @@
+"""
+The 7 classes of skin cancer lesions included in this dataset are:
+Melanocytic nevi (nv)
+Melanoma (mel)
+Benign keratosis-like lesions (bkl)
+Basal cell carcinoma (bcc) 
+Actinic keratoses (akiec)
+Vascular lesions (vas)
+Dermatofibroma (df)
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import seaborn as sns
+from PIL import Image
+
+np.random.seed(42)
+from sklearn.metrics import confusion_matrix,roc_curve,auc,roc_auc_score
+
+import keras
+from keras.utils.np_utils import to_categorical # used for converting labels to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator,load_img,img_to_array
+from sklearn.model_selection import train_test_split
+from scipy import stats
+from sklearn.preprocessing import LabelEncoder
+
+#for ROC
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+from sklearn.metrics import roc_auc_score
+
+SIZE=32
+
+skin_df = pd.read_csv('C:/Users/lenovo/Desktop/ARCHIVE/HAM10000_metadata_original.csv')
+
+
+#label encoding to numeric values from text
+le = LabelEncoder()
+le.fit(skin_df['dx'])
+LabelEncoder()
+print(list(le.classes_))
+ 
+skin_df['label'] = le.transform(skin_df["dx"]) 
+print(skin_df.sample(10))
+
+# Data distribution visualization
+fig = plt.figure(figsize=(12,8))
+
+ax1 = fig.add_subplot(221)
+skin_df['dx'].value_counts().plot(kind='bar', ax=ax1)
+ax1.set_ylabel('Count')
+ax1.set_title('Cell Type');
+
+ax2 = fig.add_subplot(222)
+skin_df['sex'].value_counts().plot(kind='bar', ax=ax2)
+ax2.set_ylabel('Count', size=15)
+ax2.set_title('Sex');
+
+ax3 = fig.add_subplot(223)
+skin_df['localization'].value_counts().plot(kind='bar')
+ax3.set_ylabel('Count',size=12)
+ax3.set_title('Localization')
+
+ax4 = fig.add_subplot(224)
+sample_age = skin_df[pd.notnull(skin_df['age'])]
+sns.distplot(sample_age['age'], fit=stats.norm, color='red');
+ax4.set_title('Age')
+
+plt.tight_layout()
+plt.show()
+
+# Distribution of data into various classes 
+from sklearn.utils import resample
+print(skin_df['label'].value_counts())
+
+#Balance data.
+# Many ways to balance data...weights can be assigned during model.fit
+#Each class is separated.
+
+df_0 = skin_df[skin_df['label'] == 0]
+df_1 = skin_df[skin_df['label'] == 1]
+df_2 = skin_df[skin_df['label'] == 2]
+df_3 = skin_df[skin_df['label'] == 3]
+df_4 = skin_df[skin_df['label'] == 4]
+df_5 = skin_df[skin_df['label'] == 5]
+df_6 = skin_df[skin_df['label'] == 6]
+
+n_samples=6705
+df_0_balanced = resample(df_0, replace=True, n_samples=n_samples, random_state=42) 
+df_1_balanced = resample(df_1, replace=True, n_samples=n_samples, random_state=42) 
+df_2_balanced = resample(df_2, replace=True, n_samples=n_samples, random_state=42)
+df_3_balanced = resample(df_3, replace=True, n_samples=n_samples, random_state=42)
+df_4_balanced = resample(df_4, replace=True, n_samples=n_samples, random_state=42)
+df_5_balanced = resample(df_5, replace=True, n_samples=n_samples, random_state=42)
+df_6_balanced = resample(df_6, replace=True, n_samples=n_samples, random_state=42)
+
+#Combined back to a single dataframe
+skin_df_balanced = pd.concat([df_0_balanced, df_1_balanced, 
+                              df_2_balanced, df_3_balanced, 
+                              df_4_balanced, df_5_balanced, df_6_balanced])
+
+#Check the distribution. All classes should be balanced now.
+print(skin_df_balanced['label'].value_counts())
+
+#Now time to read images based on image ID from the CSV file
+#This is the safest way to read images as it ensures the right image is read for the right ID
+image_path = {os.path.splitext(os.path.basename(x))[0]: x
+                     for x in glob(os.path.join('C:/Users/lenovo/Desktop/ARCHIVE', '*', '*.jpg'))}
+
+
+#Define the path and add as a new column
+skin_df_balanced['path'] = skin_df['image_id'].map(image_path.get)
+#Use the path to read images.
+skin_df_balanced['image'] = skin_df_balanced['path'].map(lambda x: np.asarray(Image.open(x).resize((SIZE,SIZE))))
+
+
+n_samples = 5  # number of samples for plotting
+# Plotting
+fig, m_axs = plt.subplots(7, n_samples, figsize = (4*n_samples, 3*7))
+for n_axs, (type_name, type_rows) in zip(m_axs, 
+                                         skin_df_balanced.sort_values(['dx']).groupby('dx')):
+    n_axs[0].set_title(type_name)
+    for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state=1234).iterrows()):
+        c_ax.imshow(c_row['image'])
+        c_ax.axis('off')
+        
+#Convert dataframe column of images into numpy array
+X = np.asarray(skin_df_balanced['image'].tolist())
+X = X/255.  # Scale values to 0-1. Standardscaler or other scaling methods can be used.
+Y=skin_df_balanced['label']  #Assign label values to Y
+Y_cat = to_categorical(Y, num_classes=7) #Convert to categorical as this is a multiclass classification problem
+#Split to training and testing
+x_train, x_test, y_train, y_test = train_test_split(X, Y_cat, test_size=0.2, random_state=42)
+
+
+#model definition
+
+num_classes = 7
+
+model = Sequential()
+model.add(Conv2D(256, (3, 3), activation="relu", input_shape=(SIZE, SIZE, 3)))
+#model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))  
+model.add(Dropout(0.3))
+
+model.add(Conv2D(128, (3, 3),activation='relu'))
+#model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))  
+model.add(Dropout(0.3))
+
+model.add(Conv2D(64, (3, 3),activation='relu'))
+#model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))  
+model.add(Dropout(0.3))
+model.add(Flatten())
+
+model.add(Dense(32))
+model.add(Dense(7, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['acc'])
+
+#Train
+#Augmentation during training has been skipped as of now
+
+batch_size = 16 
+epochs = 60
+
+history = model.fit(
+    x_train, y_train,
+    epochs=epochs,
+    batch_size = batch_size,
+    validation_data=(x_test, y_test),
+    verbose=2)
+
+score = model.evaluate(x_test, y_test)
+print('Test accuracy:', score[1])
+
+#training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(False)
+plt.axis(True)
+plt.show()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.grid(False)
+plt.show()
+
+
+# Prediction on test data
+y_pred = model.predict(x_test)
+# Convert predictions classes to one hot vectors 
+y_pred_classes = np.argmax(y_pred, axis = 1) 
+# Convert test data to one hot vectors
+y_true = np.argmax(y_test, axis = 1) 
+
+#Confusion matrix
+cm = confusion_matrix(y_true, y_pred_classes)
+
+print(cm)
+
+fig, ax = plt.subplots(figsize=(8,8))
+sns.set(font_scale=1)
+sns.heatmap(cm, annot=True, linewidths=.5, ax=ax)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_true, y_pred_classes))
+
+#Plot of incorrect misclassifications
+incorr_fraction = 1 - np.diag(cm) / np.sum(cm, axis=1)
+plt.bar(np.arange(7), incorr_fraction)
+plt.xlabel('True Label')
+plt.ylabel('Fraction of incorrect predictions')
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(num_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+    
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_pred.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(num_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(num_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= num_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+plt.figure()
+lw=1
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue','red','black','green','violet'])
+for i, color in zip(range(num_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([-0.05, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right",prop={'size':9})
+plt.show()
+
+roc_auc_score(y_test,y_pred)    
+
+
+
+
