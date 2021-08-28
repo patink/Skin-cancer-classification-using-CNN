2455c3eba52214b39494e4e9005dda96b323b1c7
Nisith Pati <54003224+patink@users.noreply.github.com>
2021-08-28 13:49:37 +0530

    Update dataaug.py

--- dataaug.py
+++ dataaug.py
@@ -6,7 +6,11 @@ datagen = ImageDataGenerator(
     rotation_range = 40,
     width_shift_range = 0.2,
     height_shift_range = 0.2,
-    
+    rescale = 1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest')
 #class 3
 pic = load_img('C:/Users/lenovo/Desktop/ARCHIVE/HAM10000_images_part_1/ISIC_0029297.jpg')
 pic.getpixel
@@ -17,8 +21,8 @@ pic_array = pic_array.reshape((1,)+pic_array.shape)
 pic_array.shape
 
 
-# Generate 10 images
-# batch_size: I can reduce it if lesion images are more so that it can be diversified
+# Generates 10 images
+# batch_size: can be reduced as well
 count = 0
 for batch in datagen.flow(pic_array, batch_size=1,save_to_dir="C:/Users/lenovo/Desktop/ARCHIVE/HAM10000_images_part_3", save_prefix='ISIC', save_format='jpg'):
     count += 1
